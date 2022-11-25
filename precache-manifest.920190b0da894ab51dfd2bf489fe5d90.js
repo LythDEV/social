7502718ad1908f9a780b4e9865668444c32361fb
Lyth <110655083+LythDEV@users.noreply.github.com>
2022-11-25 12:10:16 +0000

    Add files via upload

---
+++ precache-manifest.920190b0da894ab51dfd2bf489fe5d90.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "9d69aa21f19ddc4b1657",
+    "url": "/static/css/main.fac264c0.chunk.css"
+  },
+  {
+    "revision": "9d69aa21f19ddc4b1657",
+    "url": "/static/js/main.b5bf8d21.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "9a477fa746ecbe6d4483",
+    "url": "/static/js/2.86d72346.chunk.js"
+  },
+  {
+    "revision": "6732bd426c60b47172b1c7296e886a60",
+    "url": "/static/media/no-img.6732bd42.png"
+  },
+  {
+    "revision": "031ea1cf09ed652711818acec46619da",
+    "url": "/static/media/icon.031ea1cf.png"
+  },
+  {
+    "revision": "59cc29fe16c16697a2d91d0a43680d5b",
+    "url": "/index.html"
+  }
+];
